102f5f69984835c7ed61c876f1652193c663fbed
Igor Alisson <igoralissoncn@gmail.com>
2019-09-16 15:33:37 -0300

    Update climat-download.js

--- climat-download.js
+++ climat-download.js
@@ -6,11 +6,11 @@ var novoDownload = document.createElement('a');
 */ 
 
 for (var i = 0; i < el.length; i++) {
-	novoDownload.setAttribute('href','https://suap.ifrn.edu.br/edu/emitir_boletins_pdf/' + el[i].innerHTML +'/');
-	novoDownload.setAttribute('download','Boletim:' + el[i].innerHTML + '');
+    novoDownload.setAttribute('href','https://suap.ifrn.edu.br/edu/emitir_boletins_pdf/' + el[i].innerHTML +'/');
+    novoDownload.setAttribute('download','Boletim_' + el[i].innerHTML + '');
     novoDownload.click();
 
     novoDownload.setAttribute('href','https://suap.ifrn.edu.br/edu/diario_pdf/' + el[i].innerHTML +'/0/');
-    novoDownload.setAttribute('download','Diario_Completo:' + el[i].innerHTML +'');
+    novoDownload.setAttribute('download','Diario_Completo_' + el[i].innerHTML +'');
     novoDownload.click();
 }
