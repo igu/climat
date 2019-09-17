b5b3883fe850cf8854c1b1c265d969f4991c3d9d
Igor Alisson <igoralissoncn@gmail.com>
2019-09-16 23:31:51 -0300

    Alterando estrutura do código, utilizando funções setInterval e clearInterval

--- climat-download.js
+++ climat-download.js
@@ -3,14 +3,25 @@ var novoDownload = document.createElement('a');
 
 /*
     DOWNLOAD: BOLETINS com ETAPAS
-*/ 
+*/
 
-for (var i = 0; i < el.length; i++) {
-    novoDownload.setAttribute('href','https://suap.ifrn.edu.br/edu/emitir_boletins_pdf/' + el[i].innerHTML +'/');
-    novoDownload.setAttribute('download','Boletim_' + el[i].innerHTML + '');
+var i = 0;
+var downloads = setInterval(getDiarios, 3000);
+
+function getDiarios() {
+    if (i === el.length) {
+        stopDiarios();
+    }
+    novoDownload.setAttribute('href', 'https://suap.ifrn.edu.br/edu/emitir_boletins_pdf/' + el[i].innerHTML + '/');
+    novoDownload.setAttribute('download', 'Boletim_' + el[i].innerHTML + '');
     novoDownload.click();
 
-    novoDownload.setAttribute('href','https://suap.ifrn.edu.br/edu/diario_pdf/' + el[i].innerHTML +'/0/');
-    novoDownload.setAttribute('download','Diario_Completo_' + el[i].innerHTML +'');
+    novoDownload.setAttribute('href', 'https://suap.ifrn.edu.br/edu/diario_pdf/' + el[i].innerHTML + '/0/');
+    novoDownload.setAttribute('download', 'Diario_Completo_' + el[i].innerHTML + '');
     novoDownload.click();
+    i++;
+}
+
+function stopDiarios() {
+    clearInterval(downloads);
 }
