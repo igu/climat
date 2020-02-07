d08b3b4251ff896ee293263d9ff7b7e390a9b80d
Igor Alisson <igoralissoncn@gmail.com>
2020-02-07 15:53:57 -0300

    alteracoes a fim de correcoes de bugs

--- table.js
+++ table.js
@@ -14,13 +14,31 @@ function nomeMateriaFormatado(nomeMateria) {
     return materia[1].substr(1, materia[1].length - 2);
 }
 
+function qtdProfessores(index){
+    return items[index].getElementsByClassName('field-get_outras_informacoes')[0].getElementsByTagName('li').length;
+}
+
 for (var i = 0; i < items.length; i++) {
     
-    var objeto = {
+    let qtd = qtdProfessores(i);
+
+    if(qtd > 1) {
+        while ( qtd != 1) {  // mais de um professor
+            let objeto = {
+                codigo: items[i].getElementsByClassName('field-id')[0].innerText,
+                materia: nomeMateriaFormatado(items[i].getElementsByClassName('field-componente_curricular')[0].innerText),
+            }
+            qtd--;
+            objetos.push(objeto);
+        }
+    }
+
+    let objeto = {
         codigo: items[i].getElementsByClassName('field-id')[0].innerText,
         materia: nomeMateriaFormatado(items[i].getElementsByClassName('field-componente_curricular')[0].innerText),
+        professor: nomeProfessorFormatado(items[i].getElementsByClassName('field-get_outras_informacoes')[0].textContent)
     }
-    objetos.push(objeto)
+    objetos.push(objeto);
 }
 
 console.log(objetos);
