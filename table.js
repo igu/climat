af6b0a2954272649176048424f5b4d9cc8efe668
Igor Alisson <igoralissoncn@gmail.com>
2020-02-10 14:38:22 -0300

    bug disciplina por unico professor resolvido

--- table.js
+++ table.js
@@ -6,11 +6,11 @@
         4. o table.js faz o resto
 */
 
-var items = document.getElementsByTagName('tbody')[0].rows;
-var objetos = [];
+const items = document.getElementsByTagName('tbody')[0].rows;
+const objetos = [];
 
-function nomeMateriaFormatado(nomeMateria) {
-    let materia = nomeMateria.split("-");
+function nomeMateriaFormatado(nome) {
+    let materia = nome.split("-");
     return materia[1].substr(1, materia[1].length - 2);
 }
 
@@ -18,27 +18,28 @@ function qtdProfessores(index){
     return items[index].getElementsByClassName('field-get_outras_informacoes')[0].getElementsByTagName('li').length;
 }
 
-for (var i = 0; i < items.length; i++) {
+function nomeProfessorFormatado(index) {
+    let nome = items[index].getElementsByClassName('field-get_outras_informacoes')[0].textContent.split('Professores:'),
+        nome_formatado = nome[1].split('(');
+    console.log(nome_formatado[0].substr(1, nome_formatado[0].length - 2));
+    return nome_formatado[0].substr(1, nome_formatado[0].length - 2);
+}
+
+for (let i = 0; i < items.length; i++) {
     
-    let qtd = qtdProfessores(i);
+    let qtd = qtdProfessores(i),
+        codigo = items[i].getElementsByClassName('field-id')[0].innerText,
+        materia = nomeMateriaFormatado(items[i].getElementsByClassName('field-componente_curricular')[0].innerText);
 
     if(qtd > 1) {
-        while ( qtd != 1) {  // mais de um professor
-            let objeto = {
-                codigo: items[i].getElementsByClassName('field-id')[0].innerText,
-                materia: nomeMateriaFormatado(items[i].getElementsByClassName('field-componente_curricular')[0].innerText),
-            }
+      /*  while ( qtd != 0) {  // mais de um professor
+            objetos.push({ codigo, materia, professor });
             qtd--;
-            objetos.push(objeto);
-        }
+        } */
+    } else {
+        let professor = nomeProfessorFormatado(i);
+        objetos.push({ codigo, materia, professor });
     }
-
-    let objeto = {
-        codigo: items[i].getElementsByClassName('field-id')[0].innerText,
-        materia: nomeMateriaFormatado(items[i].getElementsByClassName('field-componente_curricular')[0].innerText),
-        professor: nomeProfessorFormatado(items[i].getElementsByClassName('field-get_outras_informacoes')[0].textContent)
-    }
-    objetos.push(objeto);
 }
 
 console.log(objetos);
